ad00a62896c599e62a27b1a9e499ac14af67c4fc
Florian Bussmann <hallo@florian-bussmann.de>
2025-07-25 16:37:58 +0000

    feat: random insert into timescaledb

--- collector/main.go
+++ collector/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
 
 // connect to database using a single connection
 func main() {
+	type sensorData struct {
+		Timestamp time.Time
+		DeviceId  string
+		Value     int
+	}
+
+	r := rand.New(rand.NewSource(42)) // Custom seed
+
 	/***********************************************/
 	/* Single Connection to TimescaleDB/ PostgreSQL */
 	/***********************************************/
@@ -30,4 +40,22 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(greeting)
+
+	/********************************************/
+	/* INSERT into  relational table            */
+	/********************************************/
+	var data sensorData
+
+	data.DeviceId = "1"
+	data.Timestamp = time.Now()
+	data.Value = r.Intn(100) // Random int between 0 and 99
+
+	queryInsertMetadata := `INSERT INTO sensor_data (device_id, timestamp, value) VALUES ($1, $2, $3);`
+
+	_, erra := conn.Exec(ctx, queryInsertMetadata, data.DeviceId, data.Timestamp, data.Value)
+	if erra != nil {
+		fmt.Fprintf(os.Stderr, "Unable to insert data into database: %v\n", erra)
+		os.Exit(1)
+	}
+	fmt.Printf("Inserted sensor data (%s, %s, %d) into database \n", data.DeviceId, data.Timestamp, data.Value)
 }
